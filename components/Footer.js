c00f47e03b3593a4ccadb224973ffcf94e797aa0
Vladimir Grichina <vgrichina@gmail.com>
2023-01-17 15:42:20 -0800

    Update copyright year

--- components/Footer.js
+++ components/Footer.js
@@ -5,7 +5,7 @@ import * as React from "react";
 const Footer = (props) => {
   return (
     <footer className={styles.footer}>
-      © 2021 Human Guild — Friends of{" "}
+      © 2023 Human Guild — Friends of{" "}
       <a href="https://near.org/" target="_blank">
         NEAR
       </a>{" "}
